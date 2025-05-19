98ba40d0d83c778976153fdf68c5ac3795573a2e
William R. J. Ribeiro <william.ribeiro@gmail.com>
2025-05-19 18:26:40 +0100

    Adds ESLint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['.pnpm-store/']),
+  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+]);
